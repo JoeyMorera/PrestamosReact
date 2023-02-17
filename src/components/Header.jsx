dbbb44dba1226b92e87abad81136a6ae27a6c551
Jocsan Morera <joey_morera13@hotmail.com>
2023-02-16 20:21:44 -0600

    prestamosReact version 1.0

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+function Header() {
+    return (
+        <div className="my-2 max-w-lg mx-auto bg-white shadow p-10">
+            <h1 className="text-4xl font-extrabold text-gray-500 text-center">
+                Cuanto <span className="text-indigo-600">dinero </span>necesitas?
+            </h1>
+        </div>
+    );
+}
+
+export default Header;
